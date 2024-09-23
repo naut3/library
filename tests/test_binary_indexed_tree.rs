1a971674d815b00cfd4991841eda27af5c3b1738
naut3 <naut3@DESKTOP-80UA2N1>
2024-09-23 19:27:55 +0900

    BinaryIndexedTreeの修正 二分探索と構築の関数を追加

--- tests/test_binary_indexed_tree.rs
+++ tests/test_binary_indexed_tree.rs
@@ -4,14 +4,21 @@ use library::binary_indexed_tree::BinaryIndexedTree;
 fn test_001_range() {
     let a = [1u32, 2, 3, 4, 5, 6, 7];
 
-    let mut bit = BinaryIndexedTree::new(7);
-
-    for i in 0..7 {
-        bit.add(i, a[i]);
-    }
+    let bit: BinaryIndexedTree<u32> = BinaryIndexedTree::from(&a);
 
     assert_eq!(bit.sum(0..=2), 6);
     assert_eq!(bit.sum(1..5), 14);
     assert_eq!(bit.sum(..4), 10);
     assert_eq!(bit.sum(3..), 22);
+}
+
+#[test]
+fn test_002_lower_bound() {
+    let a = [1, 10, 100, 1000, 10000];
+    let bit = BinaryIndexedTree::from(&a);
+
+    assert_eq!(bit.upper_bound(5), 1);
+    assert_eq!(bit.upper_bound(0), 0);
+    assert_eq!(bit.upper_bound(123), 3);
+    assert_eq!(bit.upper_bound(11), 2);
 }
